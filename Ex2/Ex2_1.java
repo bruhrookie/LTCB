bb5c7b45c219814ef68fa5da4bfc304afd4ac398
Nguyễn H. Bảo <149534728+bruhrookie@users.noreply.github.com>
2024-03-30 16:28:13 +0700

    Create Ex2_1.java

---
+++ Ex2/Ex2_1.java
@@ -0,0 +1,31 @@
+//
+
+public class Employee {
+	String name;
+	int hours;
+	
+	public Employee(String name, int hours) {
+		this.name = name;
+		this.hours = hours;
+	}
+
+	int wage() {
+		return this.hours * 12;
+	}
+	
+	double tax() {
+		return this.wage() * 0.15;
+	}
+	
+	double netpay() {
+		return this.wage() - this.tax();
+	}
+	
+	int raiseWage() {
+		return this.wage() + 14;
+	}
+
+  boolean checkOvertime() {
+    return this.hours > 100;
+  }
+}
